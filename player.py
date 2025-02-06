7756b089bb89627e151c1c285793407035dc2e79
Thomas M <tmahour@hotmail.com>
2025-02-06 17:45:12 -0500

    ugh

--- player.py
+++ player.py
@@ -4,19 +4,36 @@ class Player(Entity):
     def __init__(self, **kwargs):
         super().__init__()
         self.model = 'quad'
-        self.texture = 
+        self.texture = 'base char/idle.png'  # Sprite sheet
+        self.color = color.white
         self.scale_y = 2
         self.speed = 5
         self.health = 100
         self.armor = 10
 
+        # Sprite sheet properties
+        self.frame = 0
+        self.frames_per_row = 4  # Number of columns
+        self.frames_per_column = 3  # Number of rows
+        self.total_frames = self.frames_per_row * self.frames_per_column
 
+        # Set proper texture scaling
+        self.texture_scale = Vec2(1 / self.frames_per_row, 1 / self.frames_per_column)
+
+        self.update_uvs()
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+            
+        # Trigger animation on movement
+        if held_keys['d'] or held_keys['a'] or held_keys['w'] or held_keys['s']:
+            self.animate_sprite()
 
-    def update(self):
-        self.x += held_keys['d'] * time.dt * self.speed
-        self.x -= held_keys['a'] * time.dt * self.speed
-        self.y += held_keys['w'] * time.dt * self.speed
-        self.y -= held_keys['s'] * time.dt * self.speed
+    def update_uvs(self):
+        row = self.frame // self.frames_per_row
+        column = self.frame % self.frames_per_row
+        self.texture_offset = Vec2(column / self.frames_per_row, 1 - (row + 1) / self.frames_per_column)
+
+    def animate_sprite(self):
+        self.frame = (self.frame + 1) % self.total_frames
+        self.update_uvs()
